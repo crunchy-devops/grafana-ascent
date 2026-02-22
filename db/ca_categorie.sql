2f97439b8a5e0d47697ef467cc3ee9e33d741908
herve <dockerlite@gmail.com>
2026-02-22 11:37:31 +0100

    added more explanation on ca_categorie query

--- db/ca_categorie.sql
+++ db/ca_categorie.sql
@@ -6,3 +6,20 @@ FROM order_items oi
          JOIN orders o ON oi.order_id = o.id
 WHERE o.status = 'completed'
 GROUP BY p.category;
+
+
+-- # pour le dashboard
+SELECT
+    $__timeGroupAlias(o.order_date, $__interval) ,
+    p.category AS "metric", -- Alias interne a Grafana
+    SUM(p.price * oi.quantity) as partiel_ca
+FROM order_items oi
+    JOIN products p ON oi.product_id = p.id
+    JOIN orders o ON oi.order_id = o.id
+WHERE o.status = 'completed'
+  AND $__timeFilter(o.order_date) -- On ne compte que les ventes réussies
+  AND (p.category IN ($category) OR 'all' IN ($category))
+GROUP BY 1,2
+ORDER BY 1;
+
+-- dans display name,  mettre ${__field.labels.category}
